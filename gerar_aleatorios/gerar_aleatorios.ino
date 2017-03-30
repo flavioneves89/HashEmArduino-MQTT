a7cb3c1158aabecb266f79f44508614e4d6b226b
flavioneves89 <flavioneves89@hotmail.com>
2017-03-30 09:31:31 -0300

    Update gerar_aleatorios.ino

--- gerar_aleatorios/gerar_aleatorios.ino
+++ gerar_aleatorios/gerar_aleatorios.ino
@@ -4,15 +4,15 @@ String valorConcat;
 
 void setup(){
   
-  Serial.begin(9600);  // inicia a comunicação serial
-  randomSeed(analogRead(0)); // inicia a lista de valores aleatórios
+  Serial.begin(9600);  // Initiates serial communication
+  randomSeed(analogRead(0)); // Starts the list of random values
 }
 
 void loop(){
-  randNumber = random(100); // sorteia um número de 0 a 99
+  randNumber = random(100); // Draw a number from 0 to 99
  
  valorConcat = String(randNumber) + salt;
   
-  Serial.println(valorConcat);  // envia o valor de randNumber pela serial
+  Serial.println(valorConcat);  // Sends the value of randNumber to the serial
   delay(500); // espera 500 ms
 }
